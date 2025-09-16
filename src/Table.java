f0a43ca10025681c22a9c970d432e0c40275a509
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-09-16 11:11:38 +0200

    make reserveTable method

--- src/Table.java
+++ src/Table.java
@@ -1,16 +1,18 @@
 public class Table {
    private boolean isBooked;
    private int tableCode;
-   private String isAvailable;
+
 
    public Table(int tableCode, boolean isBooked) {
        this.tableCode = tableCode;
        this.isBooked = isBooked;
-       this.isAvailable = isBooked ? "Yes" : "No";
+
    }
 
    public void reserveTable(){
-
+       if(isBooked){
+           isBooked = false;
+       }
    }
 
 }
