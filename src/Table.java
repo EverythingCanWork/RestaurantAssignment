54a321e4e536f59d743335e9a642fafa98e319e4
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-09-19 11:03:23 +0200

    Make an if sats to check if input number is in range of tables amount

--- src/Table.java
+++ src/Table.java
@@ -6,11 +6,6 @@ public class Table {
    private int tableCode;
    private String customerName;
 
-   public Table(int tableCode, boolean isBooked, String customerName) {
-       this.tableCode = tableCode;
-       this.isBooked = isBooked;
-       this.customerName = customerName;
-   }
     public Table(int tableCode) {
         this.tableCode = tableCode;
         this.isBooked = false;
@@ -20,9 +15,7 @@ public class Table {
    public int getTableCode() {
        return tableCode;
    }
-   public void setTableCode(int tableCode) {
-       this.tableCode = tableCode;
-   }
+
    public void setCustomerName(String customerName) {
       this.customerName = customerName;
    }
